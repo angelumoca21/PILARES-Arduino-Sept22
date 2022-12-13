e7db42db6983c139b8de0d5929756a220acd5de4
ANGEL URIEL MORALES CARDOSO <angelmoca21@gmail.com>
2022-12-13 12:30:42 -0600

    Update s22.ino

--- s22/s22.ino
+++ s22/s22.ino
@@ -1,61 +1,63 @@
-#include<IRremote.h>
+#include <IRremote.h>
 //PINES DE CONTROL DEL MOTOR
-#define pinVelocidad 3
 #define input1 4
 #define input2 5
+#define pinVel 3
 //PINES DE CONTROL DEL RECEPTOR IR
 #define IRpin 9
-#define b123 210 //girar en sentido izq
-#define bpuntitos 206 //girar en otro sentido izq
-#define bregresar 88 //stop
-#define bhome 121 //+vel
-#define bplay 185 //-vel
+#define bizq 210   //girar en sentido izq
+#define bder 206   //girar en otro sentido izq
+#define bplay 88   //stop
+#define bvolM 121  //+vel
+#define bvolm 185  //-vel
 
-int velocidad = 255;
+int vel = 255;
 
-void setup()
-{
+void setup() {
   Serial.begin(9600);
   IrReceiver.begin(IRpin);
+  pinMode(pinVel, OUTPUT);
   pinMode(input1, OUTPUT);
   pinMode(input2, OUTPUT);
 }
 
-void loop()
+void loop() 
 {
-  while (IrReceiver.decode() != 0)
+  while (IrReceiver.decode() != 0) 
   {
-    Serial.println(IrReceiver.decodedIRData.command);
-    if (IrReceiver.decodedIRData.command == b123)
-    {
+    analogWrite(pinVel, vel);
+    if (IrReceiver.decodedIRData.command == bizq){
       digitalWrite(input1, 1);
       digitalWrite(input2, 0);
-      analogWrite(pinVelocidad, velocidad);
       Serial.println("Giro en un sentido");
+      Serial.print("Velocidad actual: ");
+      Serial.println(vel);
     }
-    else if (IrReceiver.decodedIRData.command == bpuntitos)
-    {
+    else if (IrReceiver.decodedIRData.command == bder) {
       digitalWrite(input1, 0);
       digitalWrite(input2, 1);
-      analogWrite(pinVelocidad, velocidad);
       Serial.println("Giro en otro sentido");
+      Serial.print("Velocidad actual: ");
+      Serial.println(vel);
     }
-    else if (IrReceiver.decodedIRData.command == bregresar)
-    {
+    else if (IrReceiver.decodedIRData.command == bplay) {
       digitalWrite(input1, 0);
       digitalWrite(input2, 0);
       Serial.println("Stop");
-    }
-    /*else if (IrReceiver.decodedIRData.command == bhome)
+      Serial.print("Velocidad actual: ");
+      Serial.println(vel);
+    } 
+    else if (IrReceiver.decodedIRData.command == bvolM) {
+      vel = vel + 10;
+      Serial.print("Velocidad actual: ");
+      Serial.println(vel);
+    } 
+    else if (IrReceiver.decodedIRData.command == bvolm) 
     {
-      velocidad = velocidad + 10;
-      Serial.println(velocidad);
+      vel = vel - 5;
+      Serial.print("Velocidad actual: ");
+      Serial.println(vel);
     }
-    else if (IrReceiver.decodedIRData.command == bplay)
-    {
-      velocidad = velocidad - 10;
-      Serial.println(velocidad);
-    }*/
     IrReceiver.resume();
   }
 }
